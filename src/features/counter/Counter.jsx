e955adb768e72a0c5d657cc08a8338e98b1358f0
dani <danifromecuador@gmail.com>
2023-05-15 20:46:34 -0500

    Add a feature to increment or decrement by a inputed ammount

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,15 +1,27 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
+import { useState } from "react";
 
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const dispatch = useDispatch();
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0;
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  }
   return (
     <section>
       <p>{count}</p>
       <div>
         <button onClick={() => dispatch(decrement())}>-</button>
         <button onClick={() => dispatch(increment())}>+</button>
+      </div>
+      <input type="text" value={incrementAmount} onChange={(e) => setIncrementAmount(e.target.value)} />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>add</button>
+        <button onClick={() => dispatch(resetAll)}>Reset</button>
       </div>
     </section>
   );
